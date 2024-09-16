03c1d99fb9129255ce4b96d62306d4a65359a86e
maximegii <maxime.girardet@ecole-isitech.fr>
2024-09-16 12:39:55 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Creation de projet NodeJS
+
+pour créer le projet 
+
+````bash
+npm init -y
+````
+
+Installation de la librairie dotenv:
+
+````bash
+npm install dotenv
+````
---
+++ server.js
@@ -0,0 +1,17 @@
+import "dotenv/config"
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import fs from "fs";
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+//console.log("process.env.GOOGLE_API_KEY", process.env.GOOGLE_API_KEY);
+
+async function run() {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+    const prompt = "3"
+
+    const result = await model.generateContent(prompt);
+
+    console.log("voici le resultat", result.response.text());
+
+}
+run();
