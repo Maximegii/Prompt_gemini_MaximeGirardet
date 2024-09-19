317d969eb0226af17249270d7fdd685d559cf26b
maximegii <maxime.girardet@ecole-isitech.fr>
2024-09-19 10:08:21 +0200

    fix

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,8 +1,8 @@
-// File: C:\Users\maxime.girardet\Documents\Cours ISITECH\Prompt enginering\Prompt_Gemini\gemini-prompt\src\app\page.js
-import * as entry from '../../../src/app/page.js'
+// File: C:\Users\maxime.girardet\Documents\Cours ISITECH\Prompt enginering\Prompt_Gemini\gemini-prompt\app\page.js
+import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../src/app/page.js')
+type TEntry = typeof import('../../../app/page.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
